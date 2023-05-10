5524c13efef794c316bb8d65909214a796e3f849
jackan214 <jacob.hell11@gmail.com>
2023-05-10 22:14:54 +0200

    Fixed indentation

--- src/components/Homescreen.tsx
+++ src/components/Homescreen.tsx
@@ -4,27 +4,26 @@ import { Jokebuttons } from "./Jokebuttons";
 import { TextBox } from "./Textbox";
 
 export function Homescreen() {
-    const [selectedJoke, setSelectedJokeType] = useState<string>("");
+  const [selectedJoke, setSelectedJokeType] = useState<string>("");
 
-    const handleButtonClick = (jokeType: SetStateAction<string>): void => {
-      let category = jokeType;
-      fetch(`https://v2.jokeapi.dev/joke/${category}`)
-        .then((response) => response.json())
-        .then((data) =>
-          data.type === "twopart"
-            ? setSelectedJokeType(`${data.setup}\n${data.delivery}`)
-            : setSelectedJokeType(data.joke)
-        );
-    };
+  const handleButtonClick = (jokeType: SetStateAction<string>): void => {
+    let category = jokeType;
+    fetch(`https://v2.jokeapi.dev/joke/${category}`)
+      .then((response) => response.json())
+      .then((data) =>
+        data.type === "twopart"
+          ? setSelectedJokeType(`${data.setup}\n${data.delivery}`)
+          : setSelectedJokeType(data.joke)
+      );
+  };
 
-
-    return (
-        <Container maxWidth="sm">
-        <Typography variant="h4" component="h1" align="center" paddingTop="30px">
-            What joke are you in the mood for?
-        </Typography>
-        <Jokebuttons onButtonClick={handleButtonClick} />
-        <TextBox onJokeSubmission={selectedJoke} />
-        </Container>
-    );
+  return (
+    <Container maxWidth="sm">
+      <Typography variant="h4" component="h1" align="center" paddingTop="30px">
+        What joke are you in the mood for?
+      </Typography>
+      <Jokebuttons onButtonClick={handleButtonClick} />
+      <TextBox onJokeSubmission={selectedJoke} />
+    </Container>
+  );
 }
--- src/components/Jokebuttons.tsx
+++ src/components/Jokebuttons.tsx
@@ -1,39 +1,39 @@
 import { Button, Grid } from "@mui/material";
 
-export function Jokebuttons(props: { onButtonClick: (arg0: string) => void; }) {
-    const handleClick = (jokeType: string) => {
-        props.onButtonClick(jokeType);
-    };
+export function Jokebuttons(props: { onButtonClick: (arg0: string) => void }) {
+  const handleClick = (jokeType: string) => {
+    props.onButtonClick(jokeType);
+  };
 
-    return (
-        <Grid container spacing={2} paddingTop="2rem" justifyContent="center">
-        <Grid item>
-            <Button
-            variant="contained"
-            color="primary"
-            onClick={() => handleClick("Programming")}
-            >
-            Programming
-            </Button>
-        </Grid>
-        <Grid item>
-            <Button
-            variant="contained"
-            color="primary"
-            onClick={() => handleClick("Pun")}
-            >
-            Pun
-            </Button>
-        </Grid>
-        <Grid item>
-            <Button
-            variant="contained"
-            color="primary"
-            onClick={() => handleClick("Spooky")}
-            >
-            spooky
-            </Button>
-        </Grid>
-        </Grid>
-    );
+  return (
+    <Grid container spacing={2} paddingTop="2rem" justifyContent="center">
+      <Grid item>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => handleClick("Programming")}
+        >
+          Programming
+        </Button>
+      </Grid>
+      <Grid item>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => handleClick("Pun")}
+        >
+          Pun
+        </Button>
+      </Grid>
+      <Grid item>
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={() => handleClick("Spooky")}
+        >
+          spooky
+        </Button>
+      </Grid>
+    </Grid>
+  );
 }
